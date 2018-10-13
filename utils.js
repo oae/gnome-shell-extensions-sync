ebfe3f75dfbb6ba08cf58aee9489b96737a0259d
Alperen Elhan <alperen@elhan.org>
2018-10-13 23:41:27 +0300

    Update license to GPLv2

--- utils.js
+++ utils.js
@@ -1,7 +1,20 @@
-// Copyright (c) 2018 O. Alperen Elhan
+// Copyright (C) 2018 O. Alperen Elhan
 //
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
+// This file is part of Extensions Sync.
+//
+// Extensions Sync is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Extensions Sync is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Extensions Sync.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 const GLib = imports.gi.GLib;
 
