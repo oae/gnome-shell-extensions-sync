49de134d6029885974476892bfd0c7ff77679eb5
Alperen Elhan <alperen@elhan.io>
2020-04-02 01:32:51 +0300

    feat: add extension util methods

--- src/shell/index.ts
+++ src/shell/index.ts
@@ -1,4 +1,23 @@
 import { File, Settings } from '@imports/Gio-2.0';
+
+export enum ExtensionType {
+  SYSTEM = 1,
+  PER_USER = 2,
+}
+
+export enum ExtensionState {
+  ENABLED = 1,
+  DISABLED = 2,
+  ERROR = 3,
+  OUT_OF_DATE = 4,
+  DOWNLOADING = 5,
+  INITIALIZED = 6,
+
+  // Used as an error state for operations on unknown extensions,
+  // should never be in a real extensionMeta object.
+  UNINSTALLED = 99,
+}
+
 export interface ShellExtension {
   canChange: boolean;
   dir: File;
@@ -14,15 +33,34 @@ export interface ShellExtension {
     'shell-version': Array<string>;
   };
   path: string;
-  state: number;
+  state: ExtensionState;
   stateObj: any;
   stylesheet: File;
-  type: number;
+  type: ExtensionType;
   uuid: string;
 }
+export const getExtensionIds = (): Array<string> => imports.ui.main.extensionManager.getUuids();
+
+export const getExtensionById = (extensionId: string): ShellExtension =>
+  imports.ui.main.extensionManager.lookup(extensionId);
 
 export const getCurrentExtension = (): ShellExtension => imports.misc.extensionUtils.getCurrentExtension();
 
 export const getCurrentExtensionSettings = (): Settings => imports.misc.extensionUtils.getSettings();
 
 export const notify = (text: string): void => imports.ui.main.notify(text);
+
+export const getExtensionsByType = (type: ExtensionType): Array<ShellExtension> => {
+  const extensionIds = getExtensionIds();
+  const extensions = extensionIds
+    .map((id: string): any => {
+      const extension = getExtensionById(id);
+      if (extension.type === type) {
+        return extension;
+      }
+      return undefined;
+    })
+    .filter((item) => item !== undefined);
+
+  return extensions;
+};
