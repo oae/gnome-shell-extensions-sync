4da8adee60a92c48f61a82be48f5757e88b44d55
Alperen Elhan <alperen@elhan.io>
2020-04-07 00:24:30 +0300

    feat: parse extension schema xml files

--- src/shell/index.ts
+++ src/shell/index.ts
@@ -1,4 +1,8 @@
+import { parse } from 'fast-xml-parser';
 import { File, Settings } from '@imports/Gio-2.0';
+import { execute } from '../utils';
+import { file_get_contents } from '@imports/GLib-2.0';
+import { byteArray } from '@imports/Gjs';
 
 export enum ExtensionType {
   SYSTEM = 1,
@@ -39,18 +43,71 @@ export interface ShellExtension {
   type: ExtensionType;
   uuid: string;
 }
-export const getExtensionIds = (): Array<string> => imports.ui.main.extensionManager.getUuids();
+const getExtensionIds = (): Array<string> => imports.ui.main.extensionManager.getUuids();
 
-export const getExtensionById = (extensionId: string): ShellExtension =>
-  imports.ui.main.extensionManager.lookup(extensionId);
+const readSchemaAsJson = (schemaPath: string): any => {
+  const [, contents] = file_get_contents(schemaPath);
+
+  return parse(byteArray.toString(contents), { ignoreAttributes: false });
+};
+
+const getExtensionById = (extensionId: string): ShellExtension => imports.ui.main.extensionManager.lookup(extensionId);
+
+const getExtensionSchemas = (extensionId: string): any => {
+  const extension = getExtensionById(extensionId);
+
+  const { stdout } = execute(`find -L ${extension.path} -iname "*.xml" -exec grep -l "schemalist" {} +`);
+  if (!stdout) {
+    return {
+      [extension.metadata.uuid]: {},
+    };
+  }
+
+  const schemaFiles = stdout.trim().split('\n');
+
+  const foundSchemas = schemaFiles
+    .map((schemaFile) => readSchemaAsJson(schemaFile))
+    .reduce((schemaJsonAcc, schemaJson) => {
+      if (!schemaJson || !schemaJson.schemalist || !schemaJson.schemalist.schema) {
+        return schemaJsonAcc;
+      }
+
+      const schema = schemaJson.schemalist.schema;
+
+      if (Array.isArray(schema)) {
+        const multipleSchemaObj = schema.reduce((acc, schemaObj) => {
+          if (schemaObj['@_path'] && schemaObj['@_id']) {
+            return {
+              ...acc,
+              [schemaObj['@_id']]: schemaObj['@_path'],
+            };
+          }
+        }, {});
+
+        return {
+          ...multipleSchemaObj,
+          ...schemaJsonAcc,
+        };
+      } else if (schema['@_path'] && schema['@_id']) {
+        return {
+          ...schemaJsonAcc,
+          [schema['@_id']]: schema['@_path'],
+        };
+      }
+
+      return schemaJsonAcc;
+    }, {});
+
+  return {
+    [extension.metadata.uuid]: foundSchemas,
+  };
+};
 
 export const getCurrentExtension = (): ShellExtension => imports.misc.extensionUtils.getCurrentExtension();
 
 export const getCurrentExtensionSettings = (): Settings => imports.misc.extensionUtils.getSettings();
 
-export const notify = (text: string): void => imports.ui.main.notify(text);
-
-export const getExtensionsByType = (type: ExtensionType): Array<ShellExtension> => {
+export const getAllExtensions = (type: ExtensionType): Array<ShellExtension> => {
   const extensionIds = getExtensionIds();
   const extensions = extensionIds
     .map((id: string): any => {
@@ -64,3 +121,16 @@ export const getExtensionsByType = (type: ExtensionType): Array<ShellExtension> 
 
   return extensions;
 };
+
+export const getAllExtensionSchemas = (): any => {
+  const extensions = getAllExtensions(ExtensionType.PER_USER);
+
+  return extensions.reduce((extensionAcc, extension) => {
+    return {
+      ...extensionAcc,
+      ...getExtensionSchemas(extension.metadata.uuid),
+    };
+  }, {});
+};
+
+export const notify = (text: string): void => imports.ui.main.notify(text);
