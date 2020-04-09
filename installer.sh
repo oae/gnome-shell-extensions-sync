180dbfe1186d58396a4f31e9aaaa67577c5effd5
Alperen Elhan <alperen@elhan.io>
2020-04-09 20:43:29 +0300

    refactor: remove old files

--- installer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf /tmp/gnome-shell-extensions-sync
-git clone https://github.com/oae/gnome-shell-extensions-sync.git /tmp/gnome-shell-extensions-sync
-cd /tmp/gnome-shell-extensions-sync
-make install
-busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting…")'
